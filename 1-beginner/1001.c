d97e30d2bae1b6ef6ce691732bf2d747692282b9
Rafael Ribeiro <contato.rafaelgp@gmail.com>
2020-04-20 23:08:02 -0300

    Exercício 1001

---
+++ 1-beginner/1001.c
@@ -0,0 +1,34 @@
+/*
+
+Extremely Basic
+
+Read 2 variables, named A and B and make the sum of these two variables, 
+assigning its result to the variable X. Print X as shown below. 
+Print endline after the result otherwise you will get “Presentation Error”.
+
+Input
+The input file will contain 2 integer numbers.
+
+Output
+Print the letter X (uppercase) with a blank space before and after the equal 
+signal followed by the value of X, according to the following example.
+
+Obs.: don't forget the endline after all.
+
+*/
+
+#include <stdio.h>
+
+int main() {
+
+    int a, b, x;
+
+    scanf("%d", &a);
+    scanf("%d", &b);
+
+    x = a + b;
+    printf("X = %d\n", x);
+
+    return 0;
+
+}
